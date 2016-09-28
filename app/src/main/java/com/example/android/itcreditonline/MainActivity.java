9cae3a0fbdd24a031d4f6e62d41466e23303c0ff
Aydan Ahmed <aydan.ahmed1994@gmail.com>
2016-09-28 16:25:07 +0300

    Some tabs is added!

---
+++ app/src/main/java/com/example/android/itcreditonline/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.android.itcreditonline;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ProfileFragment(), "People");
+        adapter.addFragment(new ProfileFragment(), "Group");
+        adapter.addFragment(new ProfileFragment(), "Calls");
+        viewPager.setAdapter(adapter);
+    }
+
+
+}
+
+
+
---
+++ app/src/main/java/com/example/android/itcreditonline/ProfileFragment.java
@@ -0,0 +1,20 @@
+package com.example.android.itcreditonline;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Aydin on 28.9.2016 г..
+ */
+
+public class ProfileFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.profile_fragment_view, parent, false);
+    }
+}
