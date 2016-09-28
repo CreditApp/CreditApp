ece015f6aa4e1641359cb16adbf6c9f73839356d
Simo\Simeon Angelov <simeon.angelov9@gmail.com>
2016-09-28 18:33:59 +0300

    fix tab layout

--- app/src/main/java/com/example/android/itcreditonline/MainActivity.java
+++ app/src/main/java/com/example/android/itcreditonline/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.android.itcreditonline;
 
+import android.content.Context;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,41 +12,77 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-    private Toolbar toolbar;
     private TabLayout tabLayout;
-    private ViewPager viewPager;
+    private  ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-
         //View Pager
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(new CustomAdapter(getSupportFragmentManager(),getApplicationContext()));
 
-        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        //Tab layout
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
 
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+        });
 
     }
 
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new ProfileFragment(), "People");
-        adapter.addFragment(new ProfileFragment(), "Group");
-        adapter.addFragment(new ProfileFragment(), "Calls");
-        viewPager.setAdapter(adapter);
+    //adapter for view pager
+    private class CustomAdapter extends FragmentPagerAdapter {
+
+        private String fragments [] = {"Fragment 1","Fragment 2"};
+
+        CustomAdapter(FragmentManager supportFragmentManager, Context applicationContext) {
+            super(supportFragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new ProfileFragment();
+                case 1:
+                    return new ProfileFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
+
+        //set page title according to selected tab(fragment)
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragments[position];
+        }
     }
-
-
 }
+
+
+
 
 
 
