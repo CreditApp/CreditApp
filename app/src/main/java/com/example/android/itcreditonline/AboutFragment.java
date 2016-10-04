97e74a92d02e3bb7bb2da8b011802bb8028686dc
Simo\Simeon Angelov <simeon.angelov9@gmail.com>
2016-10-04 15:35:57 +0300

    edit icons and style of about fragment

--- app/src/main/java/com/example/android/itcreditonline/AboutFragment.java
+++ app/src/main/java/com/example/android/itcreditonline/AboutFragment.java
@@ -2,23 +2,69 @@ package com.example.android.itcreditonline;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 public class AboutFragment extends Fragment {
 
     private Activity activity;
+    ImageButton call;
+    ImageButton message;
+    ImageButton mail;
+    ImageButton facebook;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_about, container, false);
+        call = (ImageButton)     root.findViewById(R.id.callIcon);
+        message = (ImageButton)  root.findViewById(R.id.messageIcon);
+        mail = (ImageButton)     root.findViewById(R.id.mailIcon);
+        facebook = (ImageButton) root.findViewById(R.id.facebookIcon);
 
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:+359888757575"));
+                startActivity(intent);
+            }
+        });
+
+        message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                sendIntent.setData(Uri.parse("sms:+359888757575"));
+                startActivity(sendIntent);
+            }
+        });
+
+        mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent(Intent.ACTION_SEND);
+                startActivityForResult(sendIntent,1);
+            }
+        });
+
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(browserIntent);
+            }
+        });
+
+        return root;
     }
 
     @Override
