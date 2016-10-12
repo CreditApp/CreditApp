1074d0cbbfe648e8081d90be8b438b7a79b13ccd
Simo\Simeon Angelov <simeon.angelov9@gmail.com>
2016-10-12 15:43:45 +0300

    check for internet connection in news fragment

--- app/src/main/java/com/example/android/itcreditonline/NewsFragment.java
+++ app/src/main/java/com/example/android/itcreditonline/NewsFragment.java
@@ -2,6 +2,8 @@ package com.example.android.itcreditonline;
 
 import android.app.Activity;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,6 +12,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.android.itcreditonline.Model.FeedItem;
 import com.example.android.itcreditonline.Model.ReadRss;
@@ -20,17 +23,24 @@ import java.util.ArrayList;
 public class NewsFragment extends Fragment {
     private Activity activity;
     private RecyclerView newsRV;
+    private TextView noInternet;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_news, container, false);
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
 
         newsRV = (RecyclerView) view.findViewById(R.id.newsRecyclerView);
+        noInternet = (TextView) view.findViewById(R.id.noInternet);
 
-        ReadRss rss = new ReadRss(activity,newsRV);
-        rss.execute();
+        ReadRss rss = new ReadRss(activity, newsRV);
+        if (isNetworkAvailable()){
+            rss.execute();
+            noInternet.setVisibility(View.GONE);
+        }
+
+
         return view;
     }
 
@@ -40,5 +50,12 @@ public class NewsFragment extends Fragment {
         this.activity = (Activity) context;
     }
 
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
 
 }
